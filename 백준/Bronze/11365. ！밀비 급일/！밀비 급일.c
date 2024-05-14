cf576edc5cba6ea4fc7d045339c755216fff0081
0Ivy0 <94064380+0Ivy0@users.noreply.github.com>
2024-05-14 20:31:49 +0900

    [Bronze IV] Title: !밀비 급일, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[500] = {0,};
+    int len, i;
+    while (strcmp(str, "END") != 0)
+    {
+        gets(str);
+        len = strlen(str);
+        if (strcmp(str, "END") == 0)
+            break;
+        for (i = len - 1; i >= 0; i--)
+            printf("%c", str[i]);
+        printf("\n");
+    }
+    return 0;
+}
