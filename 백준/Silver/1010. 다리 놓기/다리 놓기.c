0ecb059126f147c362288a36706ab37e6080c8dd
0Ivy0 <94064380+0Ivy0@users.noreply.github.com>
2024-05-21 18:07:29 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+unsigned long long p(int n, int s);
+
+int main()
+{
+    int num, n, m, i;
+    scanf("%d", &num);
+    for (i = 0; i < num; i++)
+    {
+        scanf("%d %d", &n, &m);
+        if (m - n < n) n = m-n; 
+        printf("%llu\n", p(m, m - n + 1) / p(n, 1));
+    }
+    return 0;
+}
+unsigned long long p(int n, int s)
+{
+    unsigned long long a = 1;
+    unsigned long long i;
+    for (i = n; i >= s; i--)
+        a *= i;
+    return a;
+}
