a9bc7884c17a93a17861f6d001f700500448749c
0Ivy0 <94064380+0Ivy0@users.noreply.github.com>
2024-06-24 18:11:18 +0900

    [Bronze I] Title: A/B - 2, Time: 0 ms, Memory: 1116 KB, Score: 188 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.c
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.c
@@ -2,7 +2,27 @@
 
 int main()
 {
-    float a, b;
-    scanf("%f %f", &a, &b);
-    printf("%f", a / b);
+    int a, b, i, s;
+    scanf("%d %d", &a, &b);
+    if (a == b)
+    {
+        printf("1");
+        return 0;
+    }
+    if (a > b)
+    {
+        printf("%d", a / b);
+        a -= a / b * b;
+    }
+    else printf("0");
+    if (a % b != 0) printf(".");
+    for (i = 0; i < 100; i++)
+    {
+        a *= 10;
+        s = a / b;
+        if (a % b == 0 && s == 0) break;
+        printf("%d", s);
+        a -= a / b * b;
+    }
+    return 0;
 }
