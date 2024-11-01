e86650fb60dfccd5b7e55d4b14b73b65ef7e6b44
0Ivy0 <94064380+0Ivy0@users.noreply.github.com>
2024-11-01 21:07:07 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 1156 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int arr[51][51], cnt = 0, n, m;
+
+void dfs(int y, int x)
+{
+    int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+    for (int i = 0; i < 4; i++)
+    {
+        int nx = dx[i] + x, ny = dy[i] + y;
+        if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+        if (!arr[nx][ny]) continue;
+        arr[nx][ny] = 0;
+        dfs(ny, nx);
+    }
+}
+
+int main()
+{
+    int t, k, x, y;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d %d", &m, &n, &k);
+        for (int j = 0; j < k; j++)
+        {
+            scanf("%d %d", &x, &y);
+            arr[x][y] = 1;
+        }
+        for (int j = 0; j < m; j++)
+        {
+            for (int h = 0; h < n; h++)
+            {
+                if (arr[j][h])
+                {
+                    dfs(h, j);
+                    arr[j][h] = 0;
+                    cnt++;
+                }
+            }
+        }
+        printf("%d\n", cnt);
+        cnt = 0;
+    }
+    return 0;
+}
