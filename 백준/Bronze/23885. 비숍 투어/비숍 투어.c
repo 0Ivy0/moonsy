a3b5087b3c0444e400e7819e1988a8c2091bac49
0Ivy0 <94064380+0Ivy0@users.noreply.github.com>
2025-03-09 15:58:52 +0900

    [Bronze I] Title: 비숍 투어, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/23885. 비숍 투어/비숍 투어.c
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.c
@@ -4,6 +4,8 @@ int main()
 {
     int n, m, x, y, d_x, d_y;
     scanf("%d %d %d %d %d %d", &n, &m, &x, &y, &d_x, &d_y);
-    if ((d_x - x) % 2 == 0 && (d_y - y) % 2 == 0) printf("YES");
+    if ((n == 1 || m == 1) && (d_x != x || d_y != y)) printf("NO");
+    else if ((d_x - x) % 2 == 0 && (d_y - y) % 2 == 0) printf("YES");
+    else if ((d_x - x) % 2 != 0 && (d_y - y) % 2 != 0) printf("YES");
     else printf("NO");
 }
