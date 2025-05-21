01d541752b1eea5f9d73a8e393f8bc4d0a0eaeb6
0Ivy0 <94064380+0Ivy0@users.noreply.github.com>
2025-05-21 19:47:02 +0900

    [Gold V] Title: 용액 합성하기, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int arr[100001];
+
+int main()
+{
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int n, sum = 0, s, e, save1, save2, sum_save;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    s = 0;
+    e = n - 1;
+    sum_save = arr[s] + arr[e];
+    save1 = arr[s];
+    save2 = arr[e];
+    while (s != e)
+    {
+        sum = arr[s] + arr[e];
+        if (abs(sum_save) > abs(sum))
+        {
+            save1 = arr[s];
+            save2 = arr[e];
+            sum_save = sum;
+        }
+        if (sum < 0)
+        {
+            s++;
+        }
+        else
+        {
+            e--;
+        }
+    }
+    cout << sum_save;
+}
